4557e4d92bfb8f3b6d40e58570874141acc92647
Sandeep <sandeep@techaddict.me>
2014-01-26 15:06:23 +0530

    document Cleaner features added

--- src/main/scala/io/pilo/parse/DocumentCleaner.scala
+++ src/main/scala/io/pilo/parse/DocumentCleaner.scala
@@ -1,9 +1,12 @@
 package io.pilo.parse
 
 import io.pilo.Article
-import org.jsoup.nodes.{ TextNode, Node, Document }
+import org.jsoup.nodes.{ Element, TextNode, Node, Document }
 import org.jsoup.select.Evaluator.Tag
+import org.jsoup.select.Collector
 import scala.util.matching.Regex
+import scala.collection.JavaConversions._
+import scala.collection.mutable.ListBuffer
 
 object DocumentCleaner extends io.pilo.Configuration{
 
@@ -13,6 +16,10 @@ object DocumentCleaner extends io.pilo.Configuration{
   val naughtyIDs = "[id~=(" + removeNodes + ")]"
   val naughtyClasses = "[class~=(" + removeNodes + ")]"
   val naughtyNames = "[name~=(" + removeNodes + ")]"
+  val tabsAndNewLinesReplacements = "".r
+  val divToPElementsPattern = "<(a|blockquote|dl|div|img|ol|p|pre|table|ul)"
+  val blockElemementTags = List[Tag]("a", "blockquote", "dl", "div", "img", "ol", "p", "pre", "table", "ul")
+  val articleRootTags = List[Tag]("div", "span", "article")
 
   def clean(article: Article): Unit = {
     var docToClean = article.doc
@@ -23,7 +30,7 @@ object DocumentCleaner extends io.pilo.Configuration{
 
   def cleanArticleTags(doc: Document): Document = {
     val it = doc.getElementsByTag("article").iterator()
-    while(it.hasNext()) {
+    while (it.hasNext()) {
       val article = it.next
       Array("id", "name", "class").foreach { attr =>
         article.removeAttr(attr)
@@ -34,7 +41,7 @@ object DocumentCleaner extends io.pilo.Configuration{
 
   def cleanEmTags(doc: Document): Document = {
     val it = doc.getElementsByTag("em").iterator()
-    while(it.hasNext()) {
+    while (it.hasNext()) {
       val images = doc.getElementsByTag("img")
       if (images.size == 0)
         it.next.remove()
@@ -44,7 +51,7 @@ object DocumentCleaner extends io.pilo.Configuration{
 
   def removeDropCaps(doc: Document): Document = {
     var it = doc.select("span[class~=(dropcap|drop_cap)]").iterator()
-    while(it.hasNext()) {
+    while (it.hasNext()) {
       val tn = new TextNode(it.next.text, doc.baseUri)
       doc.replaceWith(tn)
     }
@@ -53,11 +60,11 @@ object DocumentCleaner extends io.pilo.Configuration{
 
   def removeScriptsStylesComments(doc: Document): Document = {
     var it = doc.getElementsByTag("script").iterator()
-    while(it.hasNext())
+    while (it.hasNext())
       it.next.remove()
 
     it = doc.getElementsByTag("style").iterator()
-    while(it.hasNext())
+    while (it.hasNext())
       it.next.remove()
 
     def removeComments(node: Node): Unit = {
@@ -79,15 +86,15 @@ object DocumentCleaner extends io.pilo.Configuration{
   def cleanBadTags(doc: Document): Document = {
     val children = doc.body.children
     var it = children.select(naughtyIDs).iterator()
-    while(it.hasNext)
+    while (it.hasNext)
       it.next.remove()
 
     it = children.select(naughtyClasses).iterator()
-    while(it.hasNext)
+    while (it.hasNext)
       it.next.remove()
 
     it = children.select(naughtyNames).iterator()
-    while(it.hasNext)
+    while (it.hasNext)
       it.next.remove()
 
     return doc
@@ -95,13 +102,88 @@ object DocumentCleaner extends io.pilo.Configuration{
 
   def RemoveNodeREGEX(doc: Document, pattern: String): Document = {
     var it = doc.getElementsByAttributeValueMatching("id", pattern).iterator()
-    while(it.hasNext)
+    while (it.hasNext)
       it.next.remove()
 
     it = doc.getElementsByAttributeValueMatching("class", pattern).iterator()
-    while(it.hasNext)
+    while (it.hasNext)
       it.next.remove()
 
     return doc
+  }
+
+  def replaceElementsWithPara(doc: Document, div: Element) {
+    val newDoc = new Document(doc.baseUri)
+    val newNode = newDoc.createElement("p")
+    newNode.append(div.html)
+    div.replaceWith(newNode)
+  }
+
+  def convertWantedTagsToParagraphs(doc: Document, wantedTags: Tag): Document = {
+    val it = Collector.collect(wantedTags, doc).iterator()
+    while (it.hasNext) {
+      var elem = it.next
+      if(Collector.collect(blockElemementTags, elem).isEmpty)
+        replaceElementsWithPara(doc, elem)
+      else {
+        val replacements = getReplacementNodes(doc, elem)
+        elem.children() foreach(_.remove())
+        replacements foreach(n => elem.appendChild(n))
+      }
+    }
+    return doc
+  }
+
+  def getFlushedBuffer(replacementText: String, doc: Document): Node = {
+    val newDoc = new Document(doc.baseUri)
+    val newPara = newDoc.createElement("p")
+    newPara.html(replacementText)
+    newPara
+  }
+
+  def getReplacementNodes(doc: Document, div: Element) = {
+    val replacementText = new StringBuilder
+    val nodesToReturn = new ListBuffer[Node]()
+    val nodesToRemove = new ListBuffer[Node]()
+    div.childNodes.foreach{ kid =>
+      if (kid.nodeName == "p" && replacementText.size > 0) {
+        val newNode = getFlushedBuffer(replacementText, doc)
+        nodesToRemove += newNode
+        replacementText.clear()
+        if(kid.isInstanceOf[Element])
+          nodesToReturn += kid.asInstanceOf[Element]
+      }
+      else if (kid.nodeName == "#text") {
+        val kidTextNode = kid.asInstanceOf[TextNode]
+        val kidText = kidTextNode.attr("text")
+        val replaceText = tabsAndNewLinesReplacements.replaceAll(kidText)
+        if (replaceText.trim().length > 1) {
+          var prevSibNode = kidTextNode.previousSibling
+          while (prevSibNode != null && prevSibNode.nodeName == "a" && prevSibNode.attr("grv-usedalready") != "yes") {
+            replacementText.append(" " + prevSibNode.outerHtml + " ")
+            nodesToRemove += prevSibNode
+            prevSibNode.attr("grv-usedalready", "yes")
+            prevSibNode = if (prevSibNode.previousSibling == null) null else prevSibNode.previousSibling
+          }
+          replacementText.append(replaceText)
+          var nextSibNode = kidTextNode.nextSibling()
+          while (nextSibNode != null && nextSibNode.nodeName == "a" && nextSibNode.attr("grv-usedalready") != "yes") {
+            replacementText.append(" " + nextSibNode.outerHtml + " ")
+            nodesToRemove += nextSibNode
+            nextSibNode.attr("grv-usedalready", "yes")
+            nextSibNode = if (nextSibNode.nextSibling == null) null else nextSibNode.nextSibling
+          }
+        }
+        nodesToRemove += kid
+      }
+      else
+        nodesToReturn += kid
+    }
+    if (replacementText.size > 0) {
+      nodesToReturn += getFlushedBuffer(replacementText.toString, doc)
+      replacementText.clear()
+    }
+    nodesToRemove.foreach(_.remove())
+    nodesToReturn
   }
 }
