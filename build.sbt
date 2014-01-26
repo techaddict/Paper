bbd0327139cf4b1a261591628450ba2d990fbba2
Sandeep <sandeep@techaddict.me>
2014-01-26 12:14:57 +0530

    Added util func's to DocumentCleaner

--- build.sbt
+++ build.sbt
@@ -2,4 +2,5 @@ libraryDependencies ++= Seq(
   "commons-validator" % "commons-validator" % "1.4.0",
   "org.apache.opennlp" % "opennlp-tools" % "1.5.3",
   "com.ning" % "async-http-client" % "1.7.24",
-  "org.jsoup" % "jsoup" % "1.7.3")
+  "org.jsoup" % "jsoup" % "1.7.3",
+  "net.sourceforge.cssparser" % "cssparser" % "0.9.13")
--- src/main/scala/io/pilo/parse/DocumentCleaner.scala
+++ src/main/scala/io/pilo/parse/DocumentCleaner.scala
@@ -1,9 +1,18 @@
 package io.pilo.parse
 
 import io.pilo.Article
-import org.jsoup.nodes.{ Node, Document }
+import org.jsoup.nodes.{ TextNode, Node, Document }
+import org.jsoup.select.Evaluator.Tag
+import scala.util.matching.Regex
 
 object DocumentCleaner extends io.pilo.Configuration{
+
+  val removeNodes = "^side$|combx|retweet|mediaarticlerelated|menucontainer|navbar|comment|PopularQuestions|contact|foot|footer|Footer|footnote|cnn_strycaptiontxt|links|meta$|scroll|shoutbox|sponsor" +
+  "|tags|socialnetworking|socialNetworking|cnnStryHghLght|cnn_stryspcvbx|^inset$|pagetools|post-attributes|welcome_form|contentTools2|the_answers|remember-tool-tip" +
+  "|communitypromo|runaroundLeft|subscribe|vcard|articleheadings|date|^print$|popup|author-dropdown|tools|socialtools|byline|konafilter|KonaFilter|breadcrumbs|^fn$|wp-caption-text"
+  val naughtyIDs = "[id~=(" + removeNodes + ")]"
+  val naughtyClasses = "[class~=(" + removeNodes + ")]"
+  val naughtyNames = "[name~=(" + removeNodes + ")]"
 
   def clean(article: Article): Unit = {
     var docToClean = article.doc
@@ -12,8 +21,7 @@ object DocumentCleaner extends io.pilo.Configuration{
     docToClean = removeDropCaps(docToClean)
   }
 
-  def cleanArticleTags(doc1: Document): Document = {
-    var doc = doc1
+  def cleanArticleTags(doc: Document): Document = {
     val it = doc.getElementsByTag("article").iterator()
     while(it.hasNext()) {
       val article = it.next
@@ -24,8 +32,7 @@ object DocumentCleaner extends io.pilo.Configuration{
     return doc
   }
 
-  def cleanEmTags(doc1: Document): Document = {
-    var doc = doc1
+  def cleanEmTags(doc: Document): Document = {
     val it = doc.getElementsByTag("em").iterator()
     while(it.hasNext()) {
       val images = doc.getElementsByTag("img")
@@ -35,21 +42,23 @@ object DocumentCleaner extends io.pilo.Configuration{
     return doc
   }
 
-  def removeDropCaps(doc1: Document): Document = {
-    doc1
+  def removeDropCaps(doc: Document): Document = {
+    var it = doc.select("span[class~=(dropcap|drop_cap)]").iterator()
+    while(it.hasNext()) {
+      val tn = new TextNode(it.next.text, doc.baseUri)
+      doc.replaceWith(tn)
+    }
+    return doc
   }
 
-  def removeScriptsStyles(doc1: Document): Document = {
-    var doc = doc1
+  def removeScriptsStylesComments(doc: Document): Document = {
     var it = doc.getElementsByTag("script").iterator()
-    while(it.hasNext()) {
+    while(it.hasNext())
       it.next.remove()
-    }
 
     it = doc.getElementsByTag("style").iterator()
-    while(it.hasNext()) {
+    while(it.hasNext())
       it.next.remove()
-    }
 
     def removeComments(node: Node): Unit = {
       var i = 0
@@ -64,6 +73,35 @@ object DocumentCleaner extends io.pilo.Configuration{
       }
     }
     removeComments(doc)
+    return doc
+  }
+
+  def cleanBadTags(doc: Document): Document = {
+    val children = doc.body.children
+    var it = children.select(naughtyIDs).iterator()
+    while(it.hasNext)
+      it.next.remove()
+
+    it = children.select(naughtyClasses).iterator()
+    while(it.hasNext)
+      it.next.remove()
+
+    it = children.select(naughtyNames).iterator()
+    while(it.hasNext)
+      it.next.remove()
+
+    return doc
+  }
+
+  def RemoveNodeREGEX(doc: Document, pattern: String): Document = {
+    var it = doc.getElementsByAttributeValueMatching("id", pattern).iterator()
+    while(it.hasNext)
+      it.next.remove()
+
+    it = doc.getElementsByAttributeValueMatching("class", pattern).iterator()
+    while(it.hasNext)
+      it.next.remove()
+
     return doc
   }
 }
